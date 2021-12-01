3baf3373fb3efb66fcfbe77efab2271e8ceee329
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-11-30 23:07:51 -0700

    Update FractiontoDec.go

--- FractiontoDec.go
+++ FractiontoDec.go
@@ -1,6 +1,17 @@
+// First, we check to see if the number is an integer (remainder of numerator/denominator is 0) if it is,
+//we simply will return the number.
+//Otherwise we check to see if one of the numbers but not both numbers is a negative. 
+//Next we add a - if this is the case.
+// Then we can add our integer to the result because we know it cannot be repeating.
+// Now, we have our declared a map for long remainder to it's position in result.
+// From here, we can get the first remainder and add a 0 to it by multiplying it by 10.
+// We know the next digit is the remainder/denominator. 
+//If we have already seen the remainder we add the ( to the position we saw it, 
+//and then add the ) at the end and return the value. Otherwise, we put the remainder at the current length and add our next digit, we can then get our next remainder.
 
 
-
+//time complexity: O(len(frac))
+//space complexity: O(n)
 
 func fractionToDecimal(numerator int, denominator int) string {
  res := ""
