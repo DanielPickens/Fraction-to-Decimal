946943f21c35e98c2f982b0888346af8ce422fef
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-12-13 08:20:37 -0700

    Update FractiontoDec.go

--- FractiontoDec.go
+++ FractiontoDec.go
@@ -59,3 +59,4 @@ Details
 Runtime: 0 ms, faster than 100.00% of Go online submissions for Fraction to Recurring Decimal.
 Memory Usage: 2.9 MB, less than 44.44% of Go online submissions for Fraction to Recurring Decimal.
 */
+
